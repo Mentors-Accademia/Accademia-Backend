c1cdaf522ef694dbaa80056ad5723f4d52b43fea
unknown <Mentors-Accademia>
2024-01-16 07:07:41 +0500

    first

---
+++ Schema/Role.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+
+const roleSchema = new Schema({
+    role: {
+        type: String,
+
+        require: true
+    },
+    status: {
+        type: String,
+        
+        default: "Y"
+    }
+})
+
+module.exports = mongoose.model("Role", roleSchema)
